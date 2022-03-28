22feb5a701c09429873065050b2f09676dad9a70
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-28 19:09:40 +0300

    tcr: tests pass

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -76,8 +76,7 @@ function createApp(database) {
     return reduction;
   }
 
-  const isMonday = (date) =>{
-    return date.dayOfWeek === 1;
+  const isMonday = (date) =>{ return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
