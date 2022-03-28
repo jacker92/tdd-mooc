23f29ea86fcccc86afde9682b5144935697b8276
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-28 20:32:34 +0300

    tcr: tests pass

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -20,7 +20,6 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
 
-
     const date2 = parseDate2(req.query.date);
     const cost = calculateCost(age, type, date2, baseCost);
     res.json({ cost });
