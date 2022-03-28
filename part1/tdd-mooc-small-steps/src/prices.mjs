a51f255d39237ea653b0a2f50ccdb332d83319c7
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-28 20:28:32 +0300

    tcr: tests pass

--- part1/tdd-mooc-small-steps/src/prices.mjs
+++ part1/tdd-mooc-small-steps/src/prices.mjs
@@ -91,7 +91,6 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-
       const holidayPlain = Temporal.PlainDate.from(row.holiday)
       if (
         date &&
