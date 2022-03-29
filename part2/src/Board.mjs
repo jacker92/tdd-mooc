10b9b0503f3396e27183e21fc8343134fc0b4df8
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-29 18:50:23 +0300

    Refactoring

--- part2/src/Board.mjs
+++ part2/src/Board.mjs
@@ -1,19 +1,26 @@
+
+
 export class Board {
   width;
   height;
   fallingBlock;
   fallingBlockRow;
   falling;
+  stationary;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-  }
+    this.stationary = new Array(width).fill('.').map(() => new Array(height).fill('.'));
+    }
+  
 
   drop(block) {
     if (this.fallingBlock) {
       throw new Error("already falling");
     }
+
+    block.fallingBlockRow = 0;
     this.fallingBlock = block;
     this.fallingBlockRow = 0;
     this.falling = true;
@@ -36,19 +43,24 @@ export class Board {
     }
   }
 
+  isCurrentlyFallingBlock (i, j) {
+    return this.fallingBlock && this.fallingBlockRow === i && j === 1;
+  }
+
   toString() {
     let result = "";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        if (this.fallingBlock && this.fallingBlockRow === i && j === 1) {
-          result += "X";
+        if (this.isCurrentlyFallingBlock(i, j)) {
+          result += this.fallingBlock.color;
           continue;
         }
-        result += ".";
+        result += this.stationary[j][i];
       }
 
       result += "\n";
     }
     return result;
   }
+ 
 }
