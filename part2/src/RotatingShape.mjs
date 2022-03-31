669c1be7bc941b3e8fce80066830fa71b9e7e6c6
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-31 18:19:49 +0300

    rotate counter-clockwise test

--- part2/src/RotatingShape.mjs
+++ part2/src/RotatingShape.mjs
@@ -1,27 +1,38 @@
 export class RotatingShape {
   shape;
+  rows;
 
   constructor(shape) {
     this.shape = shape.replaceAll(" ", "");
+    this.rows = this.shape.split("\n")
+  }
+
+  rotateLeft() {
+    const transposed = this.transpose90DegreesCounterclockwise();
+    return this.createNewRotatingShape(transposed)
   }
 
   rotateRight() {
     const transposed = this.transpose90Degrees();
-    const transposedString = this.joinTransposedArray(transposed);
+    return this.createNewRotatingShape(transposed);
+  }
 
+  transpose90Degrees() {
+    return this.rows.map((_, index) => this.rows.map((row) => row[index]).reverse());
+  }
+
+  transpose90DegreesCounterclockwise() {
+    const reversedRows = this.rows.map(x => x.split("").reverse().join(''))
+    return reversedRows.map((_, index) => reversedRows.map((row) => row[index]));
+  }
+
+  createNewRotatingShape(transposed) {
+    const transposedString = this.joinTransposedArray(transposed);
     return new RotatingShape(transposedString);
   }
 
   joinTransposedArray(transposed) {
     return transposed.map((x) => x.join("")).join("\n");
-  }
-
-  transpose90Degrees() {
-    const splitted = this.shape.split("\n");
-
-    return splitted[0]
-      .split("")
-      .map((_, index) => splitted.map((row) => row[index]).reverse());
   }
 
   toString() {
