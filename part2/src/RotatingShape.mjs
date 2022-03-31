ad4d0e93d08231c40990cc120560b26d0d64f2d9
jacker92 <lahtinen.jaakko@gmail.com>
2022-03-31 18:20:07 +0300

    run format

--- part2/src/RotatingShape.mjs
+++ part2/src/RotatingShape.mjs
@@ -4,12 +4,12 @@ export class RotatingShape {
 
   constructor(shape) {
     this.shape = shape.replaceAll(" ", "");
-    this.rows = this.shape.split("\n")
+    this.rows = this.shape.split("\n");
   }
 
   rotateLeft() {
     const transposed = this.transpose90DegreesCounterclockwise();
-    return this.createNewRotatingShape(transposed)
+    return this.createNewRotatingShape(transposed);
   }
 
   rotateRight() {
@@ -18,12 +18,16 @@ export class RotatingShape {
   }
 
   transpose90Degrees() {
-    return this.rows.map((_, index) => this.rows.map((row) => row[index]).reverse());
+    return this.rows.map((_, index) =>
+      this.rows.map((row) => row[index]).reverse()
+    );
   }
 
   transpose90DegreesCounterclockwise() {
-    const reversedRows = this.rows.map(x => x.split("").reverse().join(''))
-    return reversedRows.map((_, index) => reversedRows.map((row) => row[index]));
+    const reversedRows = this.rows.map((x) => x.split("").reverse().join(""));
+    return reversedRows.map((_, index) =>
+      reversedRows.map((row) => row[index])
+    );
   }
 
   createNewRotatingShape(transposed) {
