9d6973a80e3cc8488dfc4bc7bc388c84aa007b5f
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-02 19:41:15 +0300

    Finished Z shape tests

--- part2/test/RotatingTetrominoes.test.mjs
+++ part2/test/RotatingTetrominoes.test.mjs
@@ -225,19 +225,19 @@ describe("The Z shape", () => {
     );
   });
 
-  xit("cannot be rotated right/clockwise", () => {
+  it("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
-      `.OO
-       .OO
-       ...`
+      `.Z.
+       ZZ.
+       Z..`
     );
   });
 
-  xit("cannot be rotated left/counter-clockwise", () => {
+  it("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
-      `.OO
-       .OO
-       ...`
+      `.Z.
+       ZZ.
+       Z..`
     );
   });
 
