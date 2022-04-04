8cf927f0ee92579f06b4edb1786b65f282c757ab
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-04 16:41:22 +0300

    Run format

--- part2/test/MovingFallingTetrominoes.test.mjs
+++ part2/test/MovingFallingTetrominoes.test.mjs
@@ -3,65 +3,50 @@ import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
 describe("Falling tetrominoes", () => {
-    let board;
-    beforeEach(() => {
-      board = new Board(10, 6);
-    });
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
 
-    it("a falling tetromino can be moved left", () => {
-        board.drop(Tetromino.T_SHAPE);
-        board.moveLeft();
+  it("a falling tetromino can be moved left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveLeft();
 
-        expect(board.toString()).to.equalShape(
-          `...T......
-           ..TTT.....
-           ..........
-           ..........
-           ..........
-           ..........`
-        );
-    });
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
 
-    xit("a falling tetromino can be moved right", () => {
-        board.drop(Tetromino.T_SHAPE);
-        board.moveRight();
+  xit("a falling tetromino can be moved right", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveRight();
 
-        expect(board.toString()).to.equalShape(
-          `.....T....
-           ....TTT...
-           ..........
-           ..........
-           ..........
-           ..........`
-        );
-    });
+    expect(board.toString()).to.equalShape(
+      `.....T....
+       ....TTT...
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
 
-    xit("a falling tetromino can be moved down", () => {
-        
-    });
+  xit("a falling tetromino can be moved down", () => {});
 
-    xit("it cannot be moved left beyond the board", () => {
-        
-    });
+  xit("it cannot be moved left beyond the board", () => {});
 
-    xit("it cannot be moved right beyond the board", () => {
-        
-    });
+  xit("it cannot be moved right beyond the board", () => {});
 
-    xit("it cannot be moved down beyond the board (will stop falling)", () => {
-        
-    });
+  xit("it cannot be moved down beyond the board (will stop falling)", () => {});
 
-    xit("it cannot be moved left through other blocks", () => {
-        
-    });
+  xit("it cannot be moved left through other blocks", () => {});
 
-    xit("it cannot be moved right through other blocks", () => {
-        
-    });
+  xit("it cannot be moved right through other blocks", () => {});
 
-    xit("it cannot be moved down through other blocks (will stop falling)", () => {
-        
-    });
-
+  xit("it cannot be moved down through other blocks (will stop falling)", () => {});
 });
