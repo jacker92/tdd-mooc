7100e4975710529d8dd8e4520b19ca2c9014da2e
jacker92 <lahtinen.jaakko@gmail.com>
2022-04-04 16:34:16 +0300

    Moving falling tetrominoes test skeletons

---
+++ part2/test/MovingFallingTetrominoes.test.mjs
@@ -0,0 +1,43 @@
+describe("Falling tetrominoes", () => {
+    let board;
+    beforeEach(() => {
+      board = new Board(10, 6);
+    });
+
+    xit("a falling tetromino can be moved left", () => {
+        
+    });
+
+    xit("a falling tetromino can be moved right", () => {
+        
+    });
+
+    xit("a falling tetromino can be moved down", () => {
+        
+    });
+
+    xit("it cannot be moved left beyond the board", () => {
+        
+    });
+
+    xit("it cannot be moved right beyond the board", () => {
+        
+    });
+
+    xit("it cannot be moved down beyond the board (will stop falling)", () => {
+        
+    });
+
+    xit("it cannot be moved left through other blocks", () => {
+        
+    });
+
+    xit("it cannot be moved right through other blocks", () => {
+        
+    });
+
+    xit("it cannot be moved down through other blocks (will stop falling)", () => {
+        
+    });
+
+});
